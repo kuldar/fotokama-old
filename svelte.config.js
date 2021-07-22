5acbbb4809109a1e9d88eb7fc42379fab07ccefd
Kuldar Kalvik <hi@kuldar.com>
2021-07-22 14:54:41 +0300

    Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 
 const config = {
-  kit: { target: '#svelte' },
+  kit: {
+    target: '#svelte',
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
+  },
   preprocess: [preprocess({ 'postcss': true })]
 }
 
